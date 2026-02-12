711bb4ba4b8d3ce1853a2b823b385005a99e440d
Samir Memmedzade <memmedzadesamir93@gmail.com>
2026-02-12 22:00:14 +0300

    Added Create Product Command Validator

--- src/Modules/Catalog/Catalog.Application/CreateProductCommand.cs
+++ src/Modules/Catalog/Catalog.Application/CreateProductCommand.cs
@@ -3,17 +3,46 @@ using BuildingBlocks.Domain;
 using Catalog.Domain.Entities;
 using Catalog.Domain.Repositories;
 using BuildingBlocks.Application;
+using FluentValidation;
 
 namespace Catalog.Application;
 
 public class CreateProductCommand : IRequest<Result>
 {
-    public string name { get; set; } = string.Empty;
-    public List<string> images { get; set; } = new();
-    public decimal price { get; set; }
-    public int stock { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public List<string> Images { get; set; } = new();
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
 }
 
+internal sealed class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(e => e.Name)
+            .NotEmpty()
+            .WithMessage("Name can't be empty")
+            .MaximumLength(200)
+            .WithMessage("Maximum length of product name is 200");
+
+        RuleForEach(e => e.Images)
+            .NotEmpty()
+            .WithMessage("Image url can't be empty")
+            .Must(uri => Uri.TryCreate(uri, UriKind.Absolute, out _))
+            .WithMessage("Image urls are invalid")
+            .When(e => e.Images != null);
+
+        RuleFor(e => e.Price)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("Price can't be negative");
+
+        RuleFor(e => e.Stock)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("Stock can't be negative");
+
+
+    }
+}
 
 internal sealed class CreateProductCommandHandler(
     IProductRepository productRepository,
@@ -23,11 +52,11 @@ internal sealed class CreateProductCommandHandler(
     public async Task<Result> Handle(CreateProductCommand request, CancellationToken cancellationToken)
     {
 
-        Product product = Product.Create(request.name, request.price, request.stock);
+        Product product = Product.Create(request.Name, request.Price, request.Stock);
 
-        if(request.images is not null && request.images.Count > 0)
+        if(request.Images is not null && request.Images.Count > 0)
         {
-            foreach(var x in request.images)
+            foreach(var x in request.Images)
             {
                 product.AddImage(x);
             }
